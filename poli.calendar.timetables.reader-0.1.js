238a77dd8fab1c819d1c8700d493039b55fa4fdc
Riccardo Novic <riccardo.novic@hotmail.it>
2017-09-19 16:41:59 +0200

    Removed loggers, small bugfix
    * Now the calendar creation and event insertion works like a charm!
    
    * Added TODOs for future development

--- poli.calendar.timetables.reader-0.1.js
+++ poli.calendar.timetables.reader-0.1.js
@@ -9,8 +9,15 @@ var days = {
  * for further documentation.
  * @param text The textual format of the timetables
  * @returns {Array}
+ * @example
+ * This is an input that can be digested from this function:
+ * 089183 - DATA BASES 2  (Professor: Braga Daniele Maria )
+ Semester: 1 Lectures start: 18/09/2017 Lectures end: 19/12/2017
+ Monday from 08:15 to 10:15, lesson in lecture theatre L.26.13 (Milano Città Studi - Via Golgi 20 - Edificio 26 - Piano Primo)
+ Tuesday from 08:15 to 10:15, lesson in lecture theatre D.0.3 (Milano Città Studi - Via Golgi 40 - Edificio 25 - Piano Seminterrato)
  */
 function getLectures(text) {
+    text = text.replace(/^\s+|\s+$/g, '');  // Removes trailing and leading whitespace
     var courses = text.split('\n\n\n');
     var lectures = [];
     courses.forEach(function (course) {
@@ -22,18 +29,23 @@ function getLectures(text) {
         });
         var lecturesDescriptions = course.split('\n').slice(2);
         lecturesDescriptions.forEach(function (line) {
-            // Day of lectures is shown only in textual format (mon, tue, ..), so to get a Date object we add to the
-            // first day of lectures the difference in days using the getDay() method.
+            /*
+            Day of lectures is shown only in textual format (mon, tue, ..), so to get a Date object we add to the
+            first day of lectures the difference in days using the getDay() method.
+            */
             var day = dates[0].addDays(days[line.slice(0, 3)] - dates[0].getDay());
             var hours = line.match(/(\d{2}:\d{2})/g).map(function (hour) {
                 var out = new Date(day.getTime());
-                out.setHours(parseInt(hour.slice(0, 2)) + 2, parseInt(hour.slice(3, 5)));
+                out.setHours(parseInt(hour.slice(0, 2)), parseInt(hour.slice(3, 5)));
                 return out;
             });
             var room = / ([0-9A-Z\.]*) \(/.exec(line)[1];
-            // the event is set with a weekly recursion, until the last date
-            // see https://tools.ietf.org/html/rfc5545#section-3.8.5.3 for recurrence rules
             var endDate = dates[1].toISOString().replace(/\W/g, '');
+            dates[1] = dates[1].addDays(1);
+            /*
+            The event is set with a weekly recursion, until the last date.
+            See https://tools.ietf.org/html/rfc5545#section-3.8.5.3 for recurrence rules
+            */
             lectures.push({
                 'summary': title,
                 'location': room,
@@ -45,14 +57,18 @@ function getLectures(text) {
                     'timeZone': 'Europe/Rome',
                     'dateTime': hours[1].toISOString()
                 },
-                'recurrence': ['RRULE:FREQ=WEEKLY;UNTIL=' + endDate.slice(0, -4) + 'Z']
+                'recurrence': ['RRULE:FREQ=WEEKLY;UNTIL=' + endDate.slice(0, -4) + 'Z']  // trims milliseconds
             });
         });
     });
-    console.log('oi' + lectures[0].recurrence)
     return lectures;
 }
 
+/**
+ * Adds the number of days passed as parameter to the Date.
+ * @param days  The number of days to add
+ * @returns {Date}
+ */
 Date.prototype.addDays = function (days) {
     var dat = new Date(this.valueOf());
     dat.setDate(dat.getDate() + days);
